2a4ce11428f0914f270051666bb81c15b73c2331
Aaron David <ajdavid128@gmail.com>
2022-09-19 16:27:51 -0500

    commit tree 0ff008565d241dce2bf30f184cab6efcc8ece789

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+const copyOfCats = cats.slice();
+
+function appendCat() {
+    copyOfCats.push("Broom");
+    return(copyOfCats);
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+const copyOfCats2 = cats.slice();
+
+function prependCat() {
+    copyOfCats2.unshift("Arnold");
+    return(copyOfCats2);
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+const copyOfCats3 = cats.slice();
+
+function removeLastCat() {
+    copyOfCats3.pop();
+    return(copyOfCats3);
+}
+
+function arrayFunction() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+const copyOfCats4 = cats.slice();
+
+function removeFirstCat() {
+    copyOfCats4.shift();
+    return(copyOfCats4);
+}
